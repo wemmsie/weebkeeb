24cb80d8cc38b6fcbd26f17568e619fd44006cec
emilyjaynes <hello@emilyjaynes.com>
2023-07-12 17:08:36 -0500

    Color scheme updated and additional layer keys

--- config.h
+++ config.h
@@ -21,9 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 #define SPLIT_LAYER_STATE_ENABLE
 #define RGB_MATRIX_KEYPRESSES
+#define USB_MAX_POWER_CONSUMPTION 500
+
 
 /* Set tapping term for tapdance */
-#define TAPPING_TERM 200
+#define TAPPING_TERM 150
+#define CAPS_WORD_IDLE_TIMEOUT 10000
 
 /* key matrix size */
 // Rows are doubled-up
@@ -60,71 +63,36 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* ws2812 RGB LED */
 #define RGB_DI_PIN E6
 #ifdef RGB_DI_PIN
-#    define RGBLED_NUM 68
-#    define RGBLED_SPLIT { 34, 34 }
-#    define RGBLIGHT_HUE_STEP 8
-#    define RGBLIGHT_SAT_STEP 8
-#    define RGBLIGHT_VAL_STEP 8
-#    define RGBLIGHT_LIMIT_VAL 120 /* The maximum brightness level */
-#    define RGBLIGHT_SLEEP         /* If defined, the RGB lighting will be switched off when the host goes to sleep */
-/*== enabled animations ==*/
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
-#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
-#    define RGBLIGHT_DEFAULT_VAL 120
-#    define RGBLIGHT_DEFAULT_MODE (RGBLIGHT_MODE_RAINBOW_SWIRL + 2)
+#   define RGBLED_NUM 68
+#   define RGBLED_SPLIT { 34, 34 }
+#   define RGBLIGHT_HUE_STEP 8
+#   define RGBLIGHT_SAT_STEP 8
+#   define RGBLIGHT_VAL_STEP 8
+#   define RGBLIGHT_EFFECT_RGB_TEST
+#   define RGBLIGHT_LAYERS_RETAIN_VAL
+
+#   define RGBLIGHT_SLEEP         /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+
+#   define RGBLIGHT_DEFAULT_VAL 130
+#   define RGBLIGHT_LIMIT_VAL 130
+
+#   define RGB_MATRIX_LAYERS_RETAIN_VAL
+#   define RGB_MATRIX_MAXIMUM_BRIGHTNESS 130
+#   define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#   define RGB_MATRIX_DEFAULT_HUE 30
+#   define RGB_MATRIX_DEFAULT_SAT 25
+#   define RGB_MATRIX_DEFAULT_VAL 100
+
+
 // RGB Matrix
 //#    ifdef RGB_MATRIX_ENABLE
-#        define ENABLE_RGB_MATRIX_ALPHAS_MODS
-#        define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#        define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-#        define ENABLE_RGB_MATRIX_BREATHING
-#        define ENABLE_RGB_MATRIX_BAND_SAT
-#        define ENABLE_RGB_MATRIX_BAND_VAL
-#        define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-#        define ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-#        define ENABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-#        define ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#        define ENABLE_RGB_MATRIX_CYCLE_ALL
-#        define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
-#        define ENABLE_RGB_MATRIX_CYCLE_UP_DOWN
-#        define ENABLE_RGB_MATRIX_RAINBOW_MOVING_CHEVRON
-#        define ENABLE_RGB_MATRIX_CYCLE_OUT_IN
-#        define ENABLE_RGB_MATRIX_CYCLE_OUT_IN_DUAL
-#        define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
-#        define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
-#        define ENABLE_RGB_MATRIX_DUAL_BEACON
-#        define ENABLE_RGB_MATRIX_RAINBOW_BEACON
-#        define ENABLE_RGB_MATRIX_RAINBOW_PINWHEELS
-// enabled only if RGB_MATRIX_FRAMEBUFFER_EFFECTS is defined
-#        define ENABLE_RGB_MATRIX_TYPING_HEATMAP
-#        define ENABLE_RGB_MATRIX_DIGITAL_RAIN
-// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-#        define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS
-#        define ENABLE_RGB_MATRIX_SPLASH
-#        define ENABLE_RGB_MATRIX_MULTISPLASH
-//#        define ENABLE_RGB_MATRIX_SOLID_SPLASH
-//#        define ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-#        define RGB_MATRIX_MAXIMUM_BRIGHTNESS 120
-#        define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
-#        define RGB_MATRIX_LED_COUNT RGBLED_NUM
-#        define RGB_MATRIX_SPLIT { 34, 34 }
-#        define RGB_DISABLE_WHEN_USB_SUSPENDED
-#        define RGB_MATRIX_KEYPRESSES
+#   define ENABLE_RGB_MATRIX_ALPHAS_MODS
+ 
+#   define RGB_MATRIX_LED_COUNT RGBLED_NUM
+#   define RGB_MATRIX_SPLIT { 34, 34 }
+#   define RGB_DISABLE_WHEN_USB_SUSPENDED true
+#   define RGB_MATRIX_KEYPRESSES
+#   define RGB_MATRIX_TIMEOUT 600000
 //#    endif
 #endif
 
--- keymap.c
+++ keymap.c
@@ -7,14 +7,49 @@ enum custom_keycodes {
     MAIN = SAFE_RANGE,
     L2,
     PWRD,
+    EMDASH,
     SCRNSHOT,
 };
 
 // Tap Dance declarations
 enum {
- TD_LSFT_CAPS,
+    TD_LSFT_CAPS,
 };
 
+const key_override_t one_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P1, KC_EXLM);  // Shift 1 is !
+const key_override_t two_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P2, KC_AT); // Shift 2 is @
+const key_override_t three_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P3, KC_HASH);  // Shift 3 is #
+const key_override_t four_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P4, KC_DLR); // Shift 4 is $
+const key_override_t five_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P5, KC_PERC); // Shift 5 is %
+const key_override_t six_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P6, KC_CIRC); // Shift 6 is ^
+const key_override_t seven_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P7, KC_AMPR); // Shift 7 is &
+const key_override_t eight_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P8, KC_ASTR); // Shift 8 is *
+const key_override_t nine_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P9, KC_LPRN); // Shift 9 is (
+const key_override_t zero_key_override =
+    ko_make_basic(MOD_MASK_SHIFT, KC_P0, KC_RPRN); // Shift 0 is )
+
+const key_override_t** key_overrides = (const key_override_t*[]){
+    &one_key_override,
+    &two_key_override,
+    &three_key_override,
+    &four_key_override,
+    &five_key_override,
+    &six_key_override,
+    &seven_key_override,
+    &eight_key_override,
+    &nine_key_override,
+    &zero_key_override,
+    NULL
+};
 
 /*#####################################
 ############## TAP DANCE ##############
@@ -35,62 +70,57 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_MAIN] = LAYOUT(
-      KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                            KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
-      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                            KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
-      KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,                           KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-      TD(TD_LSFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCTL,         KC_ENTER, KC_N, KC_M,  KC_COMM, KC_DOT, KC_SLSH, _______,
-      TT(1), KC_LWIN, KC_SPC,                                          KC_ENTER, KC_SPC, _______
-  ),
+    [_MAIN] = LAYOUT(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
+        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G,                                          KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        TD(TD_LSFT_CAPS), KC_Z, KC_X, KC_C, KC_V, KC_B, QK_CAPS_WORD_TOGGLE,            KC_ENTER, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_EQUAL,
+        TT(1), KC_LWIN, KC_SPC,                                                         KC_ENTER, KC_SPC, KC_LCTL
+    ),
 
-  [_L2] = LAYOUT(
-      KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______,
-      KC_BACKSLASH, _______, _______, KC_UP, _______, KC_MEDIA_NEXT_TRACK,               KC_AUDIO_VOL_UP, KC_KP_7, KC_KP_8, KC_KP_9, LSFT(KC_EQUAL), KC_EQUAL,
-      LSFT(KC_BACKSLASH), _______, KC_LEFT, KC_DOWN, KC_RIGHT,  KC_MEDIA_PLAY_PAUSE,           KC_AUDIO_MUTE, KC_KP_4, KC_KP_5, KC_KP_6, LSFT(KC_LEFT_BRACKET), LSFT(KC_RIGHT_BRACKET),
-      KC_LSFT, _______,KC_HOME, _______, KC_END, KC_MEDIA_PREV_TRACK, PWRD,       KC_SPC, KC_AUDIO_VOL_DOWN, KC_KP_1, KC_KP_2, KC_KP_3, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
-      _______, _______, SCRNSHOT,                                                   KC_SPC, KC_ENTER, KC_KP_0
-  )
-
+    [_L2] = LAYOUT(
+        KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                     KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL,
+        KC_BACKSLASH, LGUI(LSFT(KC_4)), _______, KC_UP, _______, KC_AUDIO_VOL_UP,       KC_MEDIA_NEXT_TRACK, KC_KP_7, KC_KP_8, KC_KP_9, _______, EMDASH,
+        LSFT(KC_BACKSLASH), _______, KC_LEFT, KC_DOWN, KC_RIGHT, KC_MEDIA_PLAY_PAUSE,   KC_AUDIO_MUTE, KC_KP_4, KC_KP_5, KC_KP_6, LSFT(KC_LEFT_BRACKET), LSFT(KC_RIGHT_BRACKET),
+        KC_LSFT, _______,KC_HOME, _______, KC_END, KC_AUDIO_VOL_DOWN, KC_SLEP,          KC_ENTER, KC_MEDIA_PREV_TRACK, KC_KP_1, KC_KP_2, KC_KP_3, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,
+        _______, _______, PWRD,                                                         KC_ENTER, KC_SPC, KC_KP_0
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-      case PWRD:
-          if (record->event.pressed) {
-              SEND_STRING("*******" SS_TAP(X_ENTER));
-          }
-          return false;
-          break;
-          
-      case SCRNSHOT: // Screenshot on Mac that will create crosshairs; click-drag required to complete screenshot
-          if (record->event.pressed) {
-            register_code(KC_LGUI);
-            register_code(KC_LSFT);
-            tap_code(KC_4);
-            unregister_code(KC_LGUI);
-            unregister_code(KC_LSFT);
-          }
-          return false;
-          break;
-  }
-  return true;
-};
+    switch (keycode) {
+        case PWRD:
+            if (record->event.pressed) {
+                // SEND_STRING("Pickles84!" SS_TAP(X_ENTER));
+                send_string_with_delay("Pickles84!" SS_TAP(X_ENTER), 40);
+            }
+            return false;
+            break;
+        case EMDASH:
+            if (record->event.pressed) {
+                send_string_with_delay(KC_LALT KC_KP_0 KC_KP_1 KC_KP_5 KC_KP_1, 40);
+            }
+            return false;
+            break;
+    }
+    return true;
+}
 
 /*#####################################
 ############ RGB LED SETUP ############
 #####################################*/
 
 uint8_t min(uint8_t a, uint8_t b) {
-   if (a < b) {
-      return a;
-   }
-   return b;
+    if (a < b) {
+        return a;
+    }
+    return b;
 }
 
 uint8_t max(uint8_t a, uint8_t b) {
-   if (a > b) {
-       return a;
-   }
+    if (a > b) {
+        return a;
+    }
     return b;
 }
 
@@ -98,10 +128,10 @@ void set_range(uint8_t start, uint8_t stop, uint8_t led_min, uint8_t led_max, ui
     if (stop < led_min || led_max < start) {
         // Out of range
         return;
-   }
+    }
     for(uint8_t i=max(start, led_min); i < min(stop, led_max); i++) {
         rgb_matrix_set_color(i, red, blue, green);
-   }
+    }
 }
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
@@ -109,39 +139,46 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         return false;
     }
 
-    if (get_highest_layer(layer_state) > 0) {
+    for (uint8_t i = led_min; i < led_max; i++) {
 
-        for (uint8_t i = led_min; i < led_max; i++) {
-            
-            if ((i >= 41 && i <= 43) || (i >= 48 && i <= 50) || (i >= 53 && i <= 55) || i == 58) {
+        if ((i >= 28 && i <= 33) || (i >= 62 && i <= 67)) {
+            // underglow
+            RGB_MATRIX_INDICATOR_SET_COLOR(i, 50, 50, 50);
+        }
+
+        if (get_highest_layer(layer_state) > 0) {
+
+            if ((i >= 41 && i <= 43) || (i >= 48 && i <= 50) || (i >= 53 && i <= 55) || i == 58 || i == 8 || (i >= 14 && i <= 16)) {
                 // numpad
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 250, 55, 55);
+                RGB_MATRIX_INDICATOR_SET_COLOR(i, 115, 7, 7);
             }
-            else if (i == 8 || (i >= 14 && i <= 16)) {
-                // arrow keys
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 250, 55, 55);
+            else if ((i >= 44 && i <= 45) || i == 10) {
+                // plus equal and screnshot key
+                RGB_MATRIX_INDICATOR_SET_COLOR(i, 133, 119, 0);
             }
-            else if ((i >= 44 && i <= 45)) {
-                // plus equal
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 198 ,107, 250);
+            else if ((i >= 56 && i <= 57) || i == 21 || i == 19) {
+                // brackets and home / end
+                RGB_MATRIX_INDICATOR_SET_COLOR(i, 17, 59, 100);
             }
-            else if ((i >= 46 && i <= 47)) {
-                // curly brackets
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 107, 129, 250);
-            }
-            else if ((i >= 56 && i <= 57)) {
-                 // brackets
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 107, 191, 250);
-            }
-            else if ( i == 0 || (i >= 11 && i <= 12)) {
-                // left modifiers
-                RGB_MATRIX_INDICATOR_SET_COLOR(i, 245, 221, 66);
+            else if ( i == 0 || (i >= 11 && i <= 12) || (i >= 46 && i <= 47)) {
+                // left modifiers and curly brackets
+                RGB_MATRIX_INDICATOR_SET_COLOR(i, 0, 110, 4);
             }
         }
     }
 
     if (host_keyboard_led_state().caps_lock) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(23, 217, 33, 33);
+        RGB_MATRIX_INDICATOR_SET_COLOR(23, 133, 7, 7);
     }
     return true;
+}
+
+void suspend_power_down_kb(void)
+{
+    rgb_matrix_set_suspend_state(true);
+}
+
+void suspend_wakeup_init_kb(void)
+{
+    rgb_matrix_set_suspend_state(false);
 }
